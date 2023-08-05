83aaad694c1b384fd2850b4c0b6c1a2f1361a6bf
gorangg <gorangajsek22@gmail.com>
2023-08-05 02:23:04 +0200

    apiTest

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,31 @@
-import { useState } from 'react';
-
-//https://docs.coincap.io/
-//https://api.coincap.io/v2/assets
+import { useEffect, useState } from 'react';
 
 function App() {
-	const [inputValue, setInputValue] = useState('');
-	const [findCurr, setFindCurr] = useState();
-	const [findRate, setFindRate] = useState();
-	const [findHistory, setFindHistory] = useState();
+	const [data, setData] = useState();
 
-	const fetchCurrency = () => {
-		fetch(`https://api.coincap.io/v2/assets/${inputValue}`)
-			.then((response) => response.json())
-			.then((data) => setFindCurr(data.data))
-			.catch((error) => console.error(error.message));
+	useEffect(() => {
+		const fetchImageOfTheDay = () => {
+			fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+				.then((response) => response.json())
+				.then((fetchedData) => setData(fetchedData))
+				.catch((error) => console.log(error));
+		};
 
-		fetch(`https://api.coincap.io/v2/rates/${inputValue}`)
-			.then((response) => response.json())
-			.then((data) => setFindRate(data.data))
-			.catch((error) => console.error(error.message));
-
-		fetch(`https://api.coincap.io/v2/assets/${inputValue}/history?interval=d1`)
-			.then((response) => response.json())
-			.then((data) => setFindHistory(data.data))
-			.catch((error) => console.error(error.message));
-	};
+		fetchImageOfTheDay();
+	}, []);
 
 	return (
-		<div>
-			<input
-				type='text'
-				placeholder='enter crypto name'
-				value={inputValue}
-				onChange={(e) => setInputValue(e.target.value)}
-			/>
-			<button onClick={fetchCurrency}>SEND REQUEST</button>
-			{findCurr && findRate && findHistory && (
-				<>
-					<p>Name: {findCurr.name}</p>
-					<p>Rank: {findCurr.rank}</p>
-					<p>Symbol: {findCurr.symbol}</p>
-					<p>Value change in the last 24 hours: {findCurr.changePercent24Hr}</p>
-					<p>Current price in USD: {findRate.rateUsd}</p>
-					<ol>
-						{' '}
-						Historical price:
-						{findHistory.slice(0, 5).map((item) => (
-							<li key={item.time}>{item.priceUsd}</li>
-						))}
-					</ol>
-				</>
+		<div className='app'>
+			<h1>Pozivi na API:</h1>
+			{/*<button onClick={() => fetchImageOfTheDay()}>Fetch image</button>*/}
+			{data ? (
+				<div>
+					<h3>{data.title}</h3>
+					<img src={data.url} alt='NASA image of the day' />
+					<p>{data.explanation}</p>
+				</div>
+			) : (
+				<p>No data.</p>
 			)}
 		</div>
 	);
