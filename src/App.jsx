dab2b3b868e582773b91a8fa232cecb1981203c6
gorangg <gorangajsek22@gmail.com>
2023-08-08 01:06:45 +0200

    apiTest - commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,64 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-	const [data, setData] = useState();
+	const [inputValue, setInputValue] = useState('');
+	const [findCurr, setFindCurr] = useState();
 
 	useEffect(() => {
-		const fetchImageOfTheDay = () => {
-			fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+		const fetchCurrency = () => {
+			fetch(`https://api.coincap.io/v2/assets?search=${inputValue}`)
 				.then((response) => response.json())
-				.then((fetchedData) => setData(fetchedData))
-				.catch((error) => console.log(error));
+				.then((data) => setFindCurr(data.data))
+				.catch((error) => console.error(error.message));
 		};
 
-		fetchImageOfTheDay();
-	}, []);
+		fetchCurrency();
+	}, [inputValue]);
 
 	return (
 		<div className='app'>
-			<h1>Pozivi na API:</h1>
-			{/*<button onClick={() => fetchImageOfTheDay()}>Fetch image</button>*/}
-			{data ? (
-				<div>
-					<h3>{data.title}</h3>
-					<img src={data.url} alt='NASA image of the day' />
-					<p>{data.explanation}</p>
+			<input
+				type='text'
+				placeholder='Search crypto'
+				value={inputValue}
+				onChange={(e) => setInputValue(e.target.value)}
+			/>
+			{findCurr ? (
+				<div className='wholeTable'>
+					<table>
+						<thead>
+							<th className='number'>Rank</th>
+							<th className='name'>Name</th>
+							<th className='rank'>Rank</th>
+							<th className='history'>History</th>
+						</thead>
+						<tbody>
+							<td>
+								{findCurr.map((item) => (
+									<tr>
+										<td className='number'>{item.rank}</td>
+									</tr>
+								))}
+							</td>
+							<td>
+								{findCurr.map((item) => (
+									<tr>
+										<td className='tData'>{item.name}</td>
+									</tr>
+								))}
+							</td>
+						</tbody>
+					</table>
+					{/* <ol className='name'>
+						{findCurr.map((item) => (
+							<li>{item.name}</li>
+						))}
+					</ol>
+					<ol className='rank'>
+						{findCurr.map((item) => (
+							<li>{item.rank}</li>
+						))}
+					</ol> */}
 				</div>
 			) : (
 				<p>No data.</p>
